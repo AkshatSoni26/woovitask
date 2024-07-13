ef0d3fc06c047ceb92d010e95b59c6417c267574
Akshat Soni <akshatsoni2604@gmail.com>
2024-07-14 00:07:33 +0530

    add buttons for navigate to another pages.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import "./App.css";
 import FirstPage from "./components/FirstPage";
 import SecondePage from "./components/SecondePage";
 import ThirdPage from "./components/ThirdPage";
+import { Button, ButtonGroup } from "@mui/material";
 
 function App() {
   const [currentPage, setCurrentPage] = useState(1);
   return (
     <div className="App">
+      <ButtonGroup variant="contained" aria-label="Basic button group" className="d-flex justify-content-center">
+        <Button onClick={() => setCurrentPage(1)}>First Page</Button>
+        <Button onClick={() => setCurrentPage(2)}>Second Page</Button>
+        <Button onClick={() => setCurrentPage(3)}>Third Page</Button>
+      </ButtonGroup>
       <div className="app-wrapper">
         <div className="card">
           <img src={require("./images/Logo.png")} alt="logo" className="logo" />
